3d1b821a0d1cdaf8cc2855c46a65c1c0cbb65a77
Ayush <sayush5191@gmail.com>
2024-05-07 23:20:56 +1000

    Read Update

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 
 def readCSV(csv):
@@ -5,10 +6,18 @@ def readCSV(csv):
     return data
 
 if __name__ == "__main__":
-    BusinessCSV = readCSV("Data/Businesses.csv")
-    IncomeCSV = readCSV("Data/Income.csv")
-    PollingPlacesCSV = readCSV("Data/PollingPlaces.csv")
-    PopulationCSV = readCSV("Data/Population.csv")
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+
+    BusinessPath = os.path.join(currentDir, "Data", "Businesses.csv")
+    IncomePath = os.path.join(currentDir, "Data", "Income.csv")
+    PollingPlacesPath = os.path.join(currentDir, "Data", "PollingPlaces2019.csv")
+    PopulationPath = os.path.join(currentDir, "Data", "Population.csv")
+
+    BusinessCSV = readCSV(BusinessPath)
+    IncomeCSV = readCSV(IncomePath)
+    PollingPlacesCSV = readCSV(PollingPlacesPath)
+    PopulationCSV = readCSV(PopulationPath)
+
     print(BusinessCSV.head())
     print(IncomeCSV.head())
     print(PollingPlacesCSV.head())
