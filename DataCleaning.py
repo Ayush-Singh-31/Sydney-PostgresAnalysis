a11c1fca4569ced6b13804c7207fdad6692c9b1c
Ayush <sayush5191@gmail.com>
2024-05-10 10:44:12 +1000

    Data Cleaning Update

--- DataCleaning.py
+++ DataCleaning.py
@@ -44,7 +44,7 @@ def pairPlots(Business, Income, PollingPlace, Population, Stops):
     sns.pairplot(Stops)
     plt.savefig('Plots/Stops.png')
 
-def cleanData(Business, Income, PollingPlace, Population, Stops):
+def cleanCSV(Business, Income, PollingPlace, Population, Stops):
 
     # Buiness
     # First we drop any duplicate files.
@@ -52,35 +52,49 @@ def cleanData(Business, Income, PollingPlace, Population, Stops):
     # From the data infromation we know that there are no missing values in the DataFrame, which is good.
 
     # Income
+    # Dropping any duplicates
+    Income = Income.drop_duplicates()
     # Changing data-fromants to appropriate datatypes
     Income['earners'] = pd.to_numeric(Income['earners'], errors='coerce')
     Income['median_age'] = pd.to_numeric(Income['median_age'], errors='coerce')
     Income['median_income'] = pd.to_numeric(Income['median_income'], errors='coerce')
     Income['mean_income'] = pd.to_numeric(Income['mean_income'], errors='coerce')
 
+    # PollingPlace
+    # Dropping any duplicates
+    PollingPlace = PollingPlace.drop_duplicates()
+
+    # Population
+    # Dropping any duplicates
+    Population = Population.drop_duplicates()
+
+    # Stops
+    # Dropping any duplicates
+    Stops = Stops.drop_duplicates()
+
     return Business, Income, PollingPlace, Population, Stops
 
+def cleanGeospatial(CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries, StopsCSV, srid):
+    
+    CatchmentPrimary['geom'] = CatchmentPrimary['geometry'].apply(lambda x: create_wkt_element(geom=x,srid=srid))  # applying the function
+    CatchmentPrimary = CatchmentPrimary.drop(columns="geometry")  # deleting the old copy
 
-def cleanGeospatial(CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries):
-    CatchmentPrimary_cleaned = CatchmentPrimary.dropna(subset=['ADD_DATE'])
-    CatchmentSecondary_cleaned = CatchmentSecondary.dropna(subset=['ADD_DATE'])
+    CatchmentFuture['geom'] = CatchmentFuture['geometry'].apply(lambda x: create_wkt_element(geom=x,srid=srid))  # applying the function
+    CatchmentFuture = CatchmentFuture.drop(columns="geometry")  # deleting the old copy
+    
+    CatchmentSecondary['geom'] = CatchmentSecondary['geometry'].apply(lambda x: create_wkt_element(geom=x,srid=srid))  # applying the function
+    CatchmentSecondary = CatchmentSecondary.drop(columns="geometry")  # deleting the old copy
 
-    CatchmentPrimary_cleaned.plot()
-    plt.savefig('Plots/CatchmentPrimary.png')
-    CatchmentSecondary_cleaned.plot()
-    plt.savefig('Plots/CatchmentSecondary.png')
-    CatchmentFuture.plot()
-    plt.savefig('Plots/CatchmentFuture.png')
-    SA2DigitalBoundaries.plot()
-    plt.savefig('Plots/SA2DigitalBoundaries.png')
+    StopsCSV['geom'] = gpd.points_from_xy(StopsCSV.stop_lon, StopsCSV.stop_lat)  # creating the geometry column
+    StopsCSV = StopsCSV.drop(columns=['stop_lat', 'stop_lon'])  # removing the old latitude/longitude fields
+    StopsCSV['geom'] = StopsCSV['geom'].apply(lambda x: WKTElement(x.wkt, srid=srid))
 
-    return CatchmentPrimary_cleaned, CatchmentSecondary_cleaned, CatchmentFuture, SA2DigitalBoundaries
+    return CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries, StopsCSV
 
 def create_wkt_element(geom, srid):
     if geom.geom_type == 'Polygon':
         geom = MultiPolygon([geom])
     return WKTElement(geom.wkt, srid)
-
 
 if __name__ == "__main__":
 
@@ -122,26 +136,5 @@ if __name__ == "__main__":
     describeData(SA2DigitalBoundaries, 'Data Description/SA2DigitalBoundariesDescription.txt')
 
     # Cleaning the data while keeping the original data-files
-    Business, Income, PollingPlace, Population, Stops = cleanData(BusinessCSV, IncomeCSV, PollingPlacesCSV, PopulationCSV, StopsCSV)
-    CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries = cleanGeospatial(CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries)
-    
-    StopsCSV['geom'] = gpd.points_from_xy(StopsCSV.stop_lon, StopsCSV.stop_lat)  # creating the geometry column
-    StopsCSV = StopsCSV.drop(columns=['stop_lat', 'stop_lon'])  # removing the old latitude/longitude fields
-    
-    StopsCSV['geom'] = StopsCSV['geom'].apply(lambda x: WKTElement(x.wkt, srid=srid))
-    print(StopsCSV.head())
-    
-    CatchmentPrimaryog = CatchmentPrimary.copy()  # creating a copy of the original for later
-    CatchmentPrimary['geom'] = CatchmentPrimary['geometry'].apply(lambda x: create_wkt_element(geom=x,srid=srid))  # applying the function
-    CatchmentPrimary = CatchmentPrimary.drop(columns="geometry")  # deleting the old copy
-    print(CatchmentPrimary.head())
-    
-    CatchmentFutureog = CatchmentFuture.copy()  # creating a copy of the original for later
-    CatchmentFuture['geom'] = CatchmentFuture['geometry'].apply(lambda x: create_wkt_element(geom=x,srid=srid))  # applying the function
-    CatchmentFuture = CatchmentFuture.drop(columns="geometry")  # deleting the old copy
-    print(CatchmentFuture.head())
-            
-    CatchmentSecondaryog = CatchmentSecondary.copy()  # creating a copy of the original for later
-    CatchmentSecondary['geom'] = CatchmentSecondary['geometry'].apply(lambda x: create_wkt_element(geom=x,srid=srid))  # applying the function
-    CatchmentSecondary = CatchmentSecondary.drop(columns="geometry")  # deleting the old copy
-    print(CatchmentSecondary.head())
+    Business, Income, PollingPlace, Population, Stops = cleanCSV(BusinessCSV, IncomeCSV, PollingPlacesCSV, PopulationCSV, StopsCSV)
+    CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries, stops = cleanGeospatial(CatchmentPrimary, CatchmentSecondary, CatchmentFuture, SA2DigitalBoundaries, Stops, srid)
