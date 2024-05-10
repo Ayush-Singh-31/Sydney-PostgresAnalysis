69b8a9bb07512df9c609106036c4fae6f5c677c7
Ayush <sayush5191@gmail.com>
2024-05-10 22:09:27 +1000

    Update

--- Testing.py
+++ Testing.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import geopandas as gpd
+import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
 from sqlalchemy.types import Integer, String
 from shapely.wkt import loads
@@ -16,6 +17,6 @@ currentDir = os.path.dirname(os.path.abspath(__file__))
 CatchmentSecondaryPath = os.path.join(currentDir, "Data", "SA2 Digital Boundaries", "SA2_2021_AUST_GDA2020.shp")
 SA2 = gpd.read_file(CatchmentSecondaryPath)
 
-SA2 = SA2.drop(columns=['geometry'])
-print(SA2)
-print(SA2[SA2['STE_NAME21'] == 'New South Wales'])
+SA2 = SA2[SA2['GCC_NAME21'] == 'Greater Sydney']
+SA2.plot()
+plt.show()
