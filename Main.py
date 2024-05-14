41ba8ba311fcec3d6fb292ebbc1a0e9821f16937
Ayush <sayush5191@gmail.com>
2024-05-15 00:01:30 +1000

    Update

--- Main.py
+++ Main.py
@@ -378,12 +378,14 @@ if __name__ == "__main__":
     importParking(currentDir, conn)
     importStairs(currentDir, conn)
     indexing(conn)
+    # print(pd.read_sql_query("SELECT * FROM buss_table order by zbusiness desc", conn))
     schema = """
-    CREATE TABLE buss_table AS
-    SELECT p.sa2_code, p.sa2_name,  (CAST(b.total_businesses AS FLOAT) / p.total_people * 1000 - AVG(CAST(b.total_businesses AS FLOAT) / p.total_people * 1000) OVER ()) / STDDEV_POP(CAST(b.total_businesses AS FLOAT) / p.total_people * 1000) OVER () AS zbusiness
-    FROM Business b JOIN Population p 
-    ON p.sa2_code = b.sa2_code 
-    WHERE industry_name = 'Electricity, Gas, Water and Waste Services'
+    CREATE TABLE stops_table AS 
+    SELECT s.SA2_CODE21, s.SA2_NAME21, (COUNT(st.stop_id) - AVG(COUNT(st.stop_id)) OVER ()) / STDDEV_POP(COUNT(st.stop_id)) OVER () AS zstops
+    FROM Stops st JOIN SA2 s ON ST_Contains(st.Geometry, s.geom)
+    GROUP BY s.SA2_CODE21
     """
     query(conn, schema)
-    print(pd.read_sql_query("SELECT * FROM buss_table order by zbusiness desc", conn))
+    
+    
+    print(pd.read_sql_query("SELECT * FROM stops_table order by zstops desc", conn))
